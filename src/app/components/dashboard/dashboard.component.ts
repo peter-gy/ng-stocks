0b2fd131fa09a42fb5d03f7f52bc652f38c57a33
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2020-07-16 17:48:38 +0200

    dashboard created

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StockInterface, StocksService } from 'src/app/services/stocks.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  stocks: Array<StockInterface>;
+  symbols: Array<string>;
 
-  constructor() { }
+  constructor(private service: StocksService) {
+    this.symbols = service.get();
+  }
 
   ngOnInit(): void {
+    this.service.load(this.symbols).subscribe(stocks => this.stocks = stocks);
   }
 
 }
